d5d58b49b422dd27f6e3298607a4557bc9a8cf9f
Lauryn <laurynsylvain07@gmail.com>
2025-11-14 10:19:20 -0500

    lab #4

--- class_4_instance_attr.py
+++ class_4_instance_attr.py
@@ -7,3 +7,8 @@ class House:
     # attribute name "size" with int value 2000
     # attribute name "owner" from argument during class calling
     # attribute name "address" from argument during class calling
+
+    def __init__ (self, owner, address):
+        self.size = 2500
+        self.owner = owner
+        self.address = address
